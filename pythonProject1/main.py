3b3db55f5bcd6d6a9d9779fef7ca73fc78cb1bc6
Nisrinaeka <72739696+Nisrinaeka@users.noreply.github.com>
2021-05-04 16:04:20 +0700

    Add files via upload

---
+++ pythonProject1/main.py
@@ -0,0 +1,33 @@
+#  <string>Data Mahasiswa</string>
+#    </property>
+#   </widget>
+#   <widget class="QPlainTextEdit" name="plainTextEdit">
+#    <property name="geometry">
+#     <rect>
+#      <x>60</x>
+#      <y>80</y>
+#      <width>471</width>
+#      <height>311</height>
+#     </rect>
+#    </property>
+#   </widget>
+#   <widget class="QLabel" name="label_2">
+#    <property name="geometry">
+#     <rect>
+# @@ -239,6 +229,16 @@
+#     <string>HAPUS</string>
+#    </property>
+#   </widget>
+#   <widget class="QListWidget" name="listWidget">
+#    <property name="geometry">
+#     <rect>
+#      <x>60</x>
+#      <y>80</y>
+#      <width>471</width>
+#      <height>311</height>
+#     </rect>
+#    </property>
+#   </widget>
+#  </widget>
+#  <resources/>
+#  <connections/>
